b41bdd7ef3415493446a2279d6d8a367d98d6e25
gayatri <gayatrinikam2750@gmail.com>
2024-03-07 17:46:42 +0530

    demo

---
+++ demo2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n1;
+char c1;
+
+printf("enter num: ");
+scanf("%d",&n1);
+
+
+printf("enter char: ");
+scanf("%c",&c1);
+//scanf("%*c %c",&c1);
+
+return 0;
+}
