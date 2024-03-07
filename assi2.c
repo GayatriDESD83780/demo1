6624aa495a35870360903a0f65b759ff987a5728
gayatri <gayatrinikam2750@gmail.com>
2024-03-07 23:10:00 +0530

    factor

---
+++ assi2.c
@@ -0,0 +1,20 @@
+
+// factorial 
+#include<stdio.h>
+int main()
+{
+int a,i,fact=1;
+printf("num to find factorial:");
+scanf("%d",&a);
+for(i=1;i<=a;i++)
+{
+fact=fact*i;
+}
+printf("factorial of %d is %d",a,fact);
+
+
+
+
+
+return 0;
+}
