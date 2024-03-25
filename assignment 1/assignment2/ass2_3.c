fb0d40ae01beec96038261ed5d5f363022e77fe4
gayatri <gayatrinikam2750@gmail.com>
2024-03-25 17:22:58 +0530

    solved assignment submitted

---
+++ assignment 1/assignment2/ass2_3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+//while(1){
+int num;
+printf("enter the number");
+scanf("%d",&num);
+if( num>0)
+printf("number is positive");
+else if( num<0)
+printf("number is negative");
+else
+printf("numer is zero 0");
+//}
+return 0;
+}
